d08471ae87365f21b7046e02d243b335dcf7b104
AlekssandraK <164624422+AlekssandraK@users.noreply.github.com>
2024-06-10 20:37:12 +0300

    go-sprint-8-final-project

---
+++ parcel.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"database/sql"
+	"errors"
+)
+
+type ParcelStore struct {
+	db *sql.DB
+}
+
+func NewParcelStore(db *sql.DB) ParcelStore {
+	return ParcelStore{db: db}
+}
+
+func (s ParcelStore) Add(p Parcel) (int, error) {
+	res, err := s.db.Exec("INSERT INTO parcel (client, status, address, created_at) VALUES (:client, :status, :address, :created_at)",
+		sql.Named("client", p.Client),
+		sql.Named("status", p.Status),
+		sql.Named("address", p.Address),
+		sql.Named("created_at", p.CreatedAt))
+	if err != nil {
+		return 0, err
+	}
+	number, err := res.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+
+	return int(number), nil
+}
+
+func (s ParcelStore) GetByClient(client int) ([]Parcel, error) {
+	row, err := s.db.Query("SELECT * FROM parcel WHERE client = :client", sql.Named("client", client))
+	if err != nil {
+		return nil, err
+	}
+	defer row.Close()
+	var res []Parcel
+	for row.Next() {
+		if err = row.Err(); err != nil {
+			return nil, err
+		}
+		p := Parcel{}
+		err := row.Scan(&p.Number, &p.Client, &p.Status, &p.Address, &p.CreatedAt)
+		if err != nil {
+			return []Parcel{}, err
+		}
+		res = append(res, p)
+	}
+	if err = row.Err(); err != nil {
+		return nil, err
+	}
+	return res, nil
+}
+
+func (s ParcelStore) GetParcelByID(parcelID int) (Parcel, error) {
+	rows := s.db.QueryRow("SELECT * FROM parcel WHERE number = :number", sql.Named("number", parcelID))
+	var p Parcel
+	switch err := rows.Scan(&p.Number, &p.Client, &p.Status, &p.Address, &p.CreatedAt); err {
+	case sql.ErrNoRows:
+		return p, errors.New("no rows")
+	case nil:
+		return p, nil
+	default:
+		return p, err
+	}
+}
+
+func (s ParcelStore) SetStatus(number int, status string) error {
+	_, err := s.db.Exec("UPDATE parcel SET status =:status WHERE number =: number", sql.Named("status", status), sql.Named("number", number))
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s ParcelStore) SetAddress(number int, address string) error {
+	_, err := s.db.Exec("UPDATE parcel SET address = :address WHERE number = :number AND status = :status",
+		sql.Named("address", address),
+		sql.Named("number", number),
+		sql.Named("status", ParcelStatusRegistered))
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s ParcelStore) Delete(number int) error {
+	_, err := s.db.Exec("DELETE FROM parcel WHERE number = :number AND status = :status", sql.Named("number", number), sql.Named("status", "sent"))
+	if err != nil {
+		return err
+	}
+	return nil
+}
---
+++ parcel_test.go
@@ -0,0 +1,109 @@
+package main
+
+import (
+	"database/sql"
+	"math/rand"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+var (
+	randSource = rand.NewSource(time.Now().UnixNano())
+	randRange  = rand.New(randSource)
+)
+
+func getTestParcel() Parcel {
+	return Parcel{
+		Client:    1000,
+		Status:    ParcelStatusRegistered,
+		Address:   "test",
+		CreatedAt: time.Now().UTC().Format(time.RFC3339),
+	}
+}
+func TestAddGetDelete(t *testing.T) {
+	db, err := sql.Open("sqlite", "tracker.db")
+	require.NoError(t, err)
+	defer db.Close()
+
+	store := NewParcelStore(db)
+	parcel := getTestParcel()
+
+	id, err := store.Add(parcel)
+	require.NoError(t, err)
+	require.NotEmpty(t, id)
+
+	reqParcel, err := store.GetParcelByID(id)
+	parcel.Number = reqParcel.Number
+	assert.NoError(t, err)
+	assert.Equal(t, parcel, reqParcel)
+
+	err = store.Delete(id)
+	require.NoError(t, err)
+	_, err = store.GetParcelByID(id)
+	require.NoError(t, err)
+
+}
+
+func TestSetAddress(t *testing.T) {
+	db, err := sql.Open("sqlite", "tracker.db")
+	require.NoError(t, err)
+
+	defer db.Close()
+	store := NewParcelStore(db)
+	parcel := getTestParcel()
+	id, err := store.Add(parcel)
+
+	require.NoError(t, err)
+	require.NotEmpty(t, id)
+
+	newAddress := "new test address"
+	err = store.SetAddress(id, newAddress)
+	require.NoError(t, err)
+
+	line, err := store.GetParcelByID(id)
+	require.NoError(t, err)
+	require.Equal(t, newAddress, line.Address)
+
+}
+func TestGetByClient(t *testing.T) {
+	db, err := sql.Open("sqlite", "tracker.db")
+	require.NoError(t, err)
+
+	defer db.Close()
+
+	store := NewParcelStore(db)
+	parcels := []Parcel{
+		getTestParcel(),
+		getTestParcel(),
+		getTestParcel(),
+	}
+
+	parcelMap := map[int]Parcel{}
+
+	client := randRange.Intn(10_000_000)
+	parcels[0].Client = client
+	parcels[1].Client = client
+	parcels[2].Client = client
+
+	for i := 0; i < len(parcels); i++ {
+		id, err := store.Add(parcels[i])
+		require.NoError(t, err)
+		require.NotEmpty(t, id)
+
+		parcels[i].Number = int(id)
+		parcelMap[int(id)] = parcels[i]
+	}
+
+	storedParcel, err := store.GetByClient(client)
+	require.NoError(t, err)
+	assert.Equal(t, len(parcels), len(storedParcel))
+	require.NotEmpty(t, storedParcel)
+
+	for _, pack := range storedParcel {
+		mapParcel := parcelMap[pack.Number]
+		require.Equal(t, pack, mapParcel)
+	}
+}
